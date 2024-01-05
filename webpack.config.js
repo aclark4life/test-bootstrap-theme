ad2fa3170cde35b00a7b74c3d4b4f36657ea07de
Alex Clark <aclark@aclark.net>
2024-01-05 14:47:01 -0500

    Update project-makefile

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
-const path = require('path');
+// webpack.config.js
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: './src/index.js',
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
 };
